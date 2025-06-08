c6ccfc7e5ad9b15eebf08df48cacf326c72579fe
Saksham Lama <sakshamlama@192.168.20.45>
2025-06-08 13:26:15 +1000

    Changes

--- pristine-smiles-app/src/Components/IntroVideo/index.jsx
+++ pristine-smiles-app/src/Components/IntroVideo/index.jsx
@@ -22,16 +22,17 @@ function IntroVideo() {
                     </div>
                     <div className="intro-text">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Vivamus lacinia odio vitae vestibulum vestibulum. 
-                            Cras venenatis euismod malesuada. Fusce et libero nec nulla 
-                            facilisis tincidunt. Sed malesuada dolor vel justo gravida, 
-                            ac suscipit orci fermentum.
+                            At Pristine Smiles, dentistry with personal touch ,we believe exceptional 
+                            dental care starts with a personal touch. Our team takes the time to truly listen, 
+                            understand your needs, and make you feel at ease every step of the way. 
+                            From routine check-ups to advanced treatments, we tailor every visit to 
+                            you—because no two smiles are the same.
                         </p>
                         <p>
-                            Aliquam erat volutpat. Aenean condimentum sem ut sapien 
-                            consequat, vel eleifend est malesuada. Proin tincidunt 
-                            felis ut mi fermentum, et volutpat ligula ultricies.
+                            With gentle care, modern technology, and a warm, welcoming atmosphere, we’re 
+                            committed to making your dental experience comfortable and rewarding. 
+                            At Pristine Smiles, you're not just a patient—you’re part of our family. 
+                            Your smile matters to us, and so does your comfort.
                         </p>
                     </div>
                 </div>
